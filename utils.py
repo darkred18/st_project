be561fd8616d42bb216577b927ed105da452d938
darkred <whybred@gmail.com>
2024-03-11 13:46:40 +0900

    clear

--- utils.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-
-# 환율 가져오기
-def get_usd_krw():
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    url = 'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD'
-    exchange =requests.get(url, headers=headers).json() 
-    return exchange[0]['basePrice']
